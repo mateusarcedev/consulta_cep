9f20347b9aa11b8cf812993f6ed0e582606f0e05
Mateus Arce <mateusarce.developer@gmail.com>
2022-07-31 18:04:30 -0400

    consulta CEP

---
+++ src/js/main.js
@@ -0,0 +1,24 @@
+const cep = document.querySelector('#cep');
+
+const showData = (result) => {
+    for(const campo in result) {
+        if(document.querySelector("#"+campo)){
+            document.querySelector("#"+campo).value = result[campo]
+         }
+    }
+}
+
+cep.addEventListener("blur", () => {
+    let search = cep.value.replace("-", "");
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'default'
+    }
+
+    fetch(`https://viacep.com.br/ws/${search}/json/`, options)
+    .then( response => {response.json()
+        .then(data => showData(data))        
+    })
+    .catch(error => console.log(`Deu erro: ${error.message}`))
+})
